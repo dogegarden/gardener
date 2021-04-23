1f1f6e6ca28a89f45d27b808b012cde40219503b
KittyX29 <krishnaramasimha12@gmail.com>
2021-04-23 14:46:37 +0530

    Update cats.js

--- commands/General/cats.js
+++ commands/General/cats.js
@@ -8,7 +8,6 @@ module.exports = {
         axios
         .get('https://api.thecatapi.com/v1/images/search')
         .then((res) => {
-          console.log('RES:', res.data[0].url)
           message.reply(res.data[0].url)
           return;
         })
