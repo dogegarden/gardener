da2b4ce75f271a8491bcf3982d8451f3a9c14ff7
KittyX29 <82328083+KittyX29@users.noreply.github.com>
2021-05-09 14:35:19 +0530

    Revert "testing"

--- commands/General/help.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "help",
-  description: "Help",
-  execute: (message, args, commands) => {
-     message.reply("Docs: https://github.com/KittyX29/gardener/blob/master/Docs/commands.md")
-  }
-
-};
