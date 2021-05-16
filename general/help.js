9c3fd0ebb7c21b36c364ed50b095b354ff36a384
KittyX29 <krishnaramasimha12@gmail.com>
2021-05-16 11:58:22 +0530

    folder mismatch

--- general/help.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "help",
-  description: "help",
-  execute: (msg, args) => {
-    msg.room.sendChatMessage("Rewriting and testing bot, no commands lel");
-  },
-};
--- math/math.js
+++
@@ -1,8 +0,0 @@
-const { calculator } = require("./functions");
-module.exports = {
-  name: "math",
-  description: "math",
-  execute: (num1, operator, num2) => {
-    console.log(calculator(num1, operator, num2));
-  },
-};
